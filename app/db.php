426403d17f87cec465e6d05dd7ccca8cb5d6d83d
Lida Rani <lidarani@yahoo.com>
2025-02-16 00:14:30 +0200

    final
    final

--- app/db.php
+++ app/db.php
@@ -1,8 +1,11 @@
 <?php
-$default_db = 'roni';
-$conn = mysqli_connect("localhost", "root", "", $default_db);
+$default_db = 'vilaroni_roni';
+$host = 'mysql-vilaroni.alwaysdata.net';
+$username = 'vilaroni_admin'; 
+$password = 'Caiorn007';
+$conn = mysqli_connect($host, $username, $password, $default_db);
 
 if($conn === false){
-    die("Eroare la conectare. " . mysqli_connect_error());
+    die("Connection Error: " . mysqli_connect_error());
 }
 ?>
